527293c12321929d0bac9c2573657eb5d53b5032
deathstroke-Assassin <moh2004amed@gmail.com>
2024-08-08 18:50:55 +0000

    g\ghgh

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,16 +3,16 @@
 const axios = require('axios');
 
 async function getswchar(movie_id) {
-	"""Fetches and prints characters from a Star Wars movie."""
 	try {
 		const { data: movieData } = await axios.get('https://swapi-api.alx-tools.com/films/${movie_id}');
 		const characters = movieData.characters;
 		for (const characterUrl of characters) {
 		      const { data: characterData } = await axios.get(characterUrl);
 		      console.log(characterData.name);
-	    } catch (error) {
-        console.error(error);
     }
+  } catch (error) {
+    console.error('Error fetching movie data:', error);
+  }
 }
 
 const movie_id = process.argv[2];
