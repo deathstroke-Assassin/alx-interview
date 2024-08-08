d35999f419c04366bd130d77ac3c1338f0428719
Deathstroke_AA <58149933+deathstroke-Assassin@users.noreply.github.com>
2024-08-08 20:22:23 +0300

    sw getchAar

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,3 +1,10 @@
-import axios from "axios"
-
-const axios = Request('')
+const axios = require('axios').default;
+async function getUsers() {
+    try {
+        const responce = await axios.get('https://swapi-api.alx-tools.com/');
+        console.log(responce);
+    } catch (error) {
+        console.error(error);
+    }
+}
+getUsers()
